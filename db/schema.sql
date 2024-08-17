e99d74b2a3d71d311440c30958fd2c9ebb932ce3
windymelt <windymelt@3qe.us>
2024-08-18 03:06:03 +0900

    safer schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,11 +7,13 @@ CREATE TABLE IF NOT EXISTS queue (
     dag_id BIGINT,
     node_id BIGINT,
     grabber_id INT,
-    prerequisite_node_ids BIGINT[],
+    prerequisite_node_ids BIGINT[] DEFAULT '{}',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     target_url TEXT NOT NULL,
     payload TEXT NOT NULL,
     status queue_status NOT NULL DEFAULT 'claimed',
-    run_after TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    run_after TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT dag_id_node_id_unique UNIQUE (dag_id, node_id),
+    CONSTRAINT node_cannot_depend_itself CHECK (node_id != ANY (prerequisite_node_ids))
 );
 CREATE INDEX queue_status_idx ON queue (status);
