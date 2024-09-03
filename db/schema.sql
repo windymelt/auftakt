57b5fa097d60b4d9d19d4c5f247bf3566ad41521
windymelt <windymelt@3qe.us>
2024-09-03 21:37:06 +0900

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- DB schema for Postgres
 
 -- queue status enum
-CREATE TYPE queue_status AS ENUM ('waiting', 'claimed', 'grabbed', 'finished');
+CREATE TYPE queue_status AS ENUM ('waiting', 'claimed', 'grabbed', 'finished', 'failed');
 CREATE TABLE IF NOT EXISTS queue (
     id BIGSERIAL PRIMARY KEY,
     dag_id BIGINT,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS queue (
     status queue_status NOT NULL DEFAULT 'claimed',
     run_after TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT dag_id_node_id_unique UNIQUE (dag_id, node_id),
-    CONSTRAINT node_cannot_depend_itself CHECK (node_id != ANY (prerequisite_node_ids))
+    CONSTRAINT node_cannot_depend_itself CHECK (prerequisite_node_ids = '{}' OR node_id != ANY (prerequisite_node_ids))
 );
 CREATE INDEX queue_status_idx ON queue (status);
 
