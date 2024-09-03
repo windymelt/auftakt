4ed576a85c3db8ace7957bff66c4e14a94c20c42
windymelt <windymelt@3qe.us>
2024-09-04 03:57:15 +0900

    privatize

--- src/main/scala/request/Requester.scala
+++ src/main/scala/request/Requester.scala
@@ -5,7 +5,7 @@ import sttp.client3._
 import sttp.client3.http4s.Http4sBackend
 import sttp.model.Uri
 
-val clientResource = Http4sBackend.usingDefaultEmberClientBuilder[IO]()
+private val clientResource = Http4sBackend.usingDefaultEmberClientBuilder[IO]()
 
 def request(uri: Uri, body: Array[Byte]): IO[Response[Either[String, String]]] =
   clientResource.use { c =>
